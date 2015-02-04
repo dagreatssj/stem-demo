4bdc664186619be27a1756b08cf6e7db95100cf2
Darrell Calderon <dagreatssj@gmail.com>
2015-02-03 22:06:36 -0800

    finishing up test by adding a few assertions

--- src/StemDemo.java
+++ src/StemDemo.java
@@ -1,6 +1,8 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -28,6 +30,10 @@ public class StemDemo {
         // Click the Pizza button on nav
         webDriver.findElement(By.xpath("//li[@id='menu_pizza']/a")).click();
 
+        // assert we have landed on the pizza menu page
+        String url = webDriver.getCurrentUrl();
+        Assert.assertTrue(url.contains("/site/menu/pizza"));
+
         WebDriverWait wait = new WebDriverWait(webDriver, 30);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//form[@id='searchForm']")));
 
@@ -47,10 +53,15 @@ public class StemDemo {
         // Submit the address entered
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='submit_buttons']/div[1]")));
         webDriver.findElement(By.xpath("//div[@id='submit_buttons']/div[1]")).click();
-
+        
         // Select the closest Pizza Hut
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='results-div']/div/div[3]/ul/li[11]/div")));
         webDriver.findElement(By.xpath("//div[@id='results-div']/div/div[3]/ul/li[11]/div")).click();
+
+        // assert deliver to location is the same as we selected
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='locationAddress']")));
+        String locationThatWasSet = webDriver.findElement(By.xpath("//div[@class='locationAddress']")).getText();
+        Assert.assertEquals("3157 middlefield rd", locationThatWasSet.toLowerCase());
 
         // Click to add a Meat Lovers Pizza
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='tile-set']/div[4]/div/div/table/tbody/tr/td[2]/form/div[2]/div[1]")));
@@ -59,5 +70,14 @@ public class StemDemo {
         // Checkout
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='cartOverlayContainer']/table[2]/tbody/tr/td[2]/div")));
         webDriver.findElement(By.xpath("//div[@class='cartOverlayContainer']/table[2]/tbody/tr/td[2]/div")).click();
+
+        // check that we have all the right conditions
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='prodPrice']")));
+        String priceOfPizza = webDriver.findElement(By.xpath("//span[@class='prodPrice']")).getText();
+        Assert.assertEquals("$14.99", priceOfPizza.toLowerCase());
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='name']")));
+        String nameOfPizza = webDriver.findElement(By.xpath("//span[@class='name']")).getText();
+        Assert.assertTrue(nameOfPizza.contains("MEAT LOVER'S"));
     }
 }
